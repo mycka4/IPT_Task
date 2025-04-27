124c9a6263c8abf7ccc390013eec8dd3c00b670b
mycka4 <[202380059@psu.palawan.edu.ph]>
2025-04-27 14:06:35 +0800

    This is Second Commit

--- pscript.py
+++ pscript.py
@@ -4,3 +4,13 @@ import time
 HOST = '127.0.0.1'
 PORT = 8888
 
+def send_request(pin):
+    data = f"magicNumber = {pin:03d}"
+    request = (
+        "POST /verify HTTP/1.1\r\n"
+        f"Host: {HOST}:{PORT}\r\n"
+        "Content-Type: application/x-www-form-urlencoded\r\n"
+        f"Content-Length: {len(data)}\r\n"
+        "Connection: close\r\n"
+        f"{data}"
+    )
